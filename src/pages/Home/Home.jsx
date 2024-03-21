b60b12bf7e6039697b187ee9a8e062944aec3f4b
EvenSalomon1 <even.salo@web.de>
2024-03-21 16:49:39 +0100

    first things Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,19 +2,19 @@ import "./Home.css";
 import Search from "../../components/Search/Search";
 import FilterButton from "../../components/FilterButton/FilterButton";
 import Categories from "../../components/Categories/Categories";
-import RenderProducts from "../../components/RenderProducts/RenderProducts";
 import Navbar from "../../components/Navbar/Navbar";
+import RenderRandom from "../../components/RenderRandom/RenderRandom";
 
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
-      <div style={{ display: "flex", gap: "1rem" }}>
+      <h1>Find your favourite Product</h1>
+      <div className="search-filter">
         <Search />
         <FilterButton />
       </div>
       <Categories />
-      <RenderProducts />
+      <RenderRandom />
       <Navbar />
     </>
   );

444624e6938101ff1463c3862d48246056f0e5bf
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-22 08:16:44 +0100

    Add Price Ranges and Styles to Popup

--- src/components/FilterPopup/FilterPopup.jsx
+++ src/components/FilterPopup/FilterPopup.jsx
@@ -7,8 +7,11 @@ const FilterPopup = () => {
   // Import Context with Categories Data
   const { categoriesData } = useContext(fetchCategoriesContext);
 
-  // State for Button Text
-  const [btnText, setBtnText] = useState("");
+  // State for Categories Buttons
+  const [catVal, setCatVal] = useState("");
+
+  // State for Price Buttons
+  const [priceVal, setPriceVal] = useState("");
 
   return (
     <main className="popup">
@@ -18,20 +21,57 @@ const FilterPopup = () => {
       </div>
       <section className="popup__categories">
         <h3>Categories</h3>
-        <div className="popup__categories__container">
+        <div>
           {categoriesData.map((cat, index) => {
             return (
               <button
                 key={index}
-                onClick={(e) => setBtnText(e.target.value)}
+                onClick={(e) => setCatVal(e.target.value)}
                 className={
-                  btnText === cat ? "popup__button--selected" : "popup__button"
+                  catVal === cat ? "popup__button--selected" : "popup__button"
                 }
                 value={cat}>
                 {cat.replace("-", " ")}
               </button>
             );
           })}
+        </div>
+      </section>
+      <section className="price">
+        <h3>Price</h3>
+        <div>
+          <button
+            onClick={(e) => setPriceVal(e.target.value)}
+            value="<20"
+            className={
+              priceVal === "<20" ? "popup__button--selected" : "popup__button"
+            }>
+            0 - 20 €
+          </button>
+          <button
+            value="<50"
+            onClick={(e) => setPriceVal(e.target.value)}
+            className={
+              priceVal === "<50" ? "popup__button--selected" : "popup__button"
+            }>
+            20 - 50 €
+          </button>
+          <button
+            value="<100"
+            onClick={(e) => setPriceVal(e.target.value)}
+            className={
+              priceVal === "<100" ? "popup__button--selected" : "popup__button"
+            }>
+            50 - 100 €
+          </button>
+          <button
+            value=">100"
+            className={
+              priceVal === ">100" ? "popup__button--selected" : "popup__button"
+            }
+            onClick={(e) => setPriceVal(e.target.value)}>
+            über 100 €
+          </button>
         </div>
       </section>
     </main>

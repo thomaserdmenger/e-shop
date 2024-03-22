6ca8d4b42aabdaa98d40eb9b13751418891b1ad1
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-22 11:24:40 +0100

    Style Filter Button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,16 +5,17 @@ import Categories from "../../components/Categories/Categories";
 import Navbar from "../../components/Navbar/Navbar";
 import RenderRandom from "../../components/RenderRandom/RenderRandom";
 import FetchCategories from "../../components/FetchCategories/FetchCategories";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  
-
   return (
     <>
       <h1>Find your favourite Product</h1>
       <div className="search-filter">
         <Search />
-        <FilterButton />
+        <Link to="/search">
+          <FilterButton />
+        </Link>
       </div>
       <FetchCategories />
       <Categories />
